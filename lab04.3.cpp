7465a831623af26ebb429a9e849e58fd5c8860e7
unknown <olha.klishchevska.ri.2022@lpnu.ua.com>
2022-10-20 00:20:14 +0300

    first commit

---
+++ lab04.3.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+int main()
+{
+    double x, xp, xk, dx, a, b, c, F;
+    cout << "xp = "; cin >> xp;
+    cout << "xk = "; cin >> xk;
+    cout << "dx = "; cin >> dx;
+    cout << "a = "; cin >> a;
+    cout << "b = "; cin >> b;
+    cout << "c = "; cin >> c;
+    cout << fixed;
+    cout << "----------------------" << endl;
+    cout << "|" << setw(7) << "x" << " |"
+        << setw(9) << "" << "F" << " |" << endl;
+    cout << "----------------------" << endl;
+    x = xp;
+    while (x <= xk)
+    {
+        if (x < 3 && b != 0)
+            F = (a * x * x) - (b * x + c);
+        if (x > 3 && b == 0)
+            F = (x - a) / (x - c);
+        if (!(x < 0 && b != 0) && !(x > 0 && b == 0))
+            F = x / c;
+        cout << "|" << setw(7) << setprecision(2) << x
+            << " |" << setw(10) << setprecision(3) << F
+            << " |" << endl;
+        x += dx;
+    }
+    cout << "----------------------" << endl;
+    return 0;
+}
